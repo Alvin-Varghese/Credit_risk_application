37395c84071bbffb62ecc625fa159ca9f1abf3fa
Alvin-Varghese <biju.alv@gmail.com>
2020-09-12 17:28:41 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 import pickle
 
 # load model
-model = pickle.load(open('D:/Code/WebApp/model.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 # app
 app = Flask(__name__)
